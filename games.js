5c5eafc9be4225043a1bcb593cd530593e327216
Yumin <yjang2022@fau.edu>
2026-01-14 15:44:50 -0500

    Original change experiment

--- games.js
+++ games.js
@@ -1,4 +1,13 @@
-
+// Image credits: Kickstarter
+const games = ` [
+  {
+    "name": "Heroes Of Mythic Americas",
+    "description": "An exciting 5e RPG supplement that heroically represents pre-Columbian American cultures and mythologies",
+    "pledged": 1572,
+    "goal": 10000,
+    "backers": 9,
+    "img": "./assets/heroes_of_mythic_americas.png"
+  },
   {
     "name": "Cube Monster",
     "description": "Be the first champion to reach the top of Mount Kubia in a solo or competitive engine building strategy game",
