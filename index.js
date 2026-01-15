28d825780d3e58c9d835d06c7d8a925279d52b59
Yumin <yjang2022@fau.edu>
2026-01-14 23:12:50 -0500

    Used filter to count the number of unfunded games

--- index.js
+++ index.js
@@ -143,7 +143,9 @@ allBtn.addEventListener("click", showAllGames);
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+const unfundedGamesCount = GAMES_JSON.filter(
+    game => game.pledged < game.goal
+).length;
 
 // create a string that explains the number of unfunded games using the ternary operator
 
